1763a9518462cd55d29a4d02fe7f6a7ab7e44aa8
zechs <vijay.vky2002@gmail.com>
2023-01-31 11:32:44 +0530

    Implemented navigation graph

---
+++ app/src/main/java/zechs/takesurvey/ui/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package zechs.takesurvey.ui.home
+
+import androidx.fragment.app.Fragment
+import zechs.takesurvey.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
--- app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
+++ app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package zechs.takesurvey
+package zechs.takesurvey.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import zechs.takesurvey.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
