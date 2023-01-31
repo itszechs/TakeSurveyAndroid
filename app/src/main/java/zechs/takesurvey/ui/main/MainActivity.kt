38c6b123f6f7f820085d23cec334dc32a9a8be07
zechs <vijay.vky2002@gmail.com>
2023-01-31 11:36:48 +0530

    implemented view binding

--- app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
+++ app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package zechs.takesurvey.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import zechs.takesurvey.R
+import androidx.appcompat.app.AppCompatActivity
+import zechs.takesurvey.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
