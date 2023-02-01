d34605e2d8776979bb61ae5743463ba1e8ed3471
zechs <vijay.vky2002@gmail.com>
2023-02-02 01:48:05 +0530

    added titles for fragments

--- app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
+++ app/src/main/java/zechs/takesurvey/ui/main/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import dagger.hilt.android.AndroidEntryPoint
 import zechs.takesurvey.R
 import zechs.takesurvey.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,18 @@ class MainActivity : AppCompatActivity() {
             R.id.mainNavHostFragment
         ) as NavHostFragment
         navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.createFragment,
+                R.id.attemptFragment,
+                R.id.resultFragment
+            )
+        )
+        NavigationUI.setupActionBarWithNavController(
+            this, navController, appBarConfiguration
+        )
 
     }
 
