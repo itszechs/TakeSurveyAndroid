0d59665cb1ed1042d63f92d3767d212b97395000
zechs <vijay.vky2002@gmail.com>
2023-02-02 12:21:37 +0530

    updated api url

--- app/src/main/java/zechs/takesurvey/utils/Constants.kt
+++ app/src/main/java/zechs/takesurvey/utils/Constants.kt
@@ -2,6 +2,7 @@ package zechs.takesurvey.utils
 
 class Constants {
     companion object {
-        const val TAKESURVEY_API = "https://takesurvey.vercel.app"
+        const val TAKESURVEY = "https://takesurvey.vercel.app"
+        const val TAKESURVEY_API = "https://takesurvey-backend.vercel.app"
     }
 }
--- app/src/main/java/zechs/takesurvey/utils/Utility.kt
+++ app/src/main/java/zechs/takesurvey/utils/Utility.kt
@@ -1,10 +1,10 @@
 package zechs.takesurvey.utils
 
-import zechs.takesurvey.utils.Constants.Companion.TAKESURVEY_API
+import zechs.takesurvey.utils.Constants.Companion.TAKESURVEY
 import java.net.URL
 
 fun extractIdFromUrl(url: String): String? {
-    val domain = URL(TAKESURVEY_API).host
+    val domain = URL(TAKESURVEY).host
     val pattern = Regex("^(http|https)://${domain}/([A-Za-z0-9]{24})(/results)?$")
     val matchResult = pattern.matchEntire(url)
     return matchResult?.groupValues?.get(2)
